ba81c85ddc774c1f52fbfb3f65f9f283d9b883e7
aiya000 <aiya000.develop@gmail.com>
2023-06-27 20:35:55 +0900

    :sparkles: Make Foo.vue

---
+++ src/components/Foo.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div>{{ state }}</div>
+    <button @click="increase">Click</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const props = defineProps<{
+  initial: number
+}>()
+
+const state = ref(props.initial)
+
+function increase(): void {
+  state.value = state.value + 1
+}
+</script>
